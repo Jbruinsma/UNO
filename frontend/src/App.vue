f151350e72afe65cf4b261547d734dd5cb27f579
Justin Bruinsma <justinabruinsma@gmail.com>
2025-11-30 12:01:17 -0500

    Remove placeholder content from App.vue and retain WebSocketTest component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,13 +4,6 @@ import WebSocketTest from './components/WebSocketTest.vue'
 
 <template>
   <div class="p-4">
-    <h1>You did it!</h1>
-    <p>
-      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-      documentation
-    </p>
-
-    <!-- Render the WebSocket test component -->
     <WebSocketTest />
   </div>
 </template>
